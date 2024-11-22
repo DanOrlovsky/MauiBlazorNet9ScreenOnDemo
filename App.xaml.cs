4a505e68cbd5a8e7b224fb9ff91e34a1109b66c7
Dan Orlovsky <d.orlovsky@dnp.imgcomm.com>
2024-11-22 08:05:47 -0500

    Initial Commit

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace MauiBlazorNet9ScreenOnDemo;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new MainPage()) { Title = "MauiBlazorNet9ScreenOnDemo" };
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiBlazorNet9ScreenOnDemo;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
